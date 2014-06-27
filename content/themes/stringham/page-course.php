3ed79ff7ac75674e4821454053ccd03dbb80e4d3
Brandon Warner <brandon@punchlinead.com>
2014-06-27 10:41:49 -0600

    removes opcache_reset from header, cleans up the template for My Courses page

--- content/themes/stringham/page-course.php
+++ content/themes/stringham/page-course.php
@@ -11,7 +11,6 @@ Template Name: Courses
  * @package Stringham
  */
 
-opcache_reset();
 
 get_header(); 
 
@@ -30,10 +29,6 @@ $user = wp_get_current_user();
 						<?php echo do_shortcode('[ld_course_list mycourses="true"]');?>
 					</div>
 					
-					<!--<div class="col-md-3">
-						<?php //echo do_shortcode('[learndash_course_progress course_id="5"]');?>
-					</div>-->
-					
 				</div><!--/row-->
                     
 			<?php endwhile; // end of the loop. ?>
@@ -43,18 +38,4 @@ $user = wp_get_current_user();
 
 <?php get_footer(); ?>
 
-<!--
-
-	<div class="profile-header"> Following <span class="badge">224</span>
-		<div class="btn-group btn-group-xs pull-right">
-			<button class="btn btn-default">Show all</button>
-		</div>
-	</div>
-	
-	
-	
-	
-	
-	
--->
 
